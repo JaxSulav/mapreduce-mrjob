122cc1f9b6bd870f92d178bfd718d205257f8c88
Sulav Shrestha <stha.sulabh@gmail.com>
2023-08-11 17:58:37 -0400

    - Add another job to find top 3 users bought products from most numbers of aisles which are distinct in their first 10 cart items

--- task.py
+++ task.py
@@ -30,4 +30,30 @@ class MRTopOrders(MRJob):
             MRStep(reducer=self.reducer_sort)
         ]
 
+class MRTopUsers(MRJob):
+
+    def mapper(self, _, line):
+        _, user_id, add_to_cart_order, aisle_id = line.split(',')
+        if int(''.join(re.findall(r'\d+', add_to_cart_order))) <= 10: # to deal with int conversion error due to data like this '"3"'
+            yield user_id, aisle_id
+
+    def reducer_count_aisles(self, user_id, aisles_ids):
+        distinct_aisles = set(aisles_ids)
+        yield None, (len(distinct_aisles), user_id)
+
+    def reducer_sort(self, _, user_aisle_counts):
+        top_users = sorted(user_aisle_counts, reverse=True)[:3]
+        for count, user_id in top_users:
+            yield user_id, count
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper,
+                   reducer=self.reducer_count_aisles),
+            MRStep(reducer=self.reducer_sort)
+        ]
+    
+if __name__ == '__main__':
+    # MRTopOrders.run()
+    MRTopUsers.run()
 
